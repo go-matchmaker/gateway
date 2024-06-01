27e7d95bd92ac26e879ef10bceb829e1c7d97912
bulutcan99 <gocerbulutcan@gmail.com>
2024-06-01 14:40:50 +0300

    feat: added router

--- internal/adapter/transport/http/router.go
+++ internal/adapter/transport/http/router.go
@@ -14,6 +14,6 @@ func (s *server) authRouter() {
 }
 
 func (s *server) hrManagementRouter() {
-	route := s.app.Group("/hr-management")
-	route.Post("/create-user", s.CreateUser, s.RateLimiter(5, time.Minute), s.GetUserDetail, s.GetUserDetail)
+	route := s.app.Group("/hr-management", s.GetUserDetail, s.HRPermission)
+	route.Post("/create-user", s.CreateUser, s.RateLimiter(5, time.Minute))
 }

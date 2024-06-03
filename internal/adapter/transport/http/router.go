fb970bab4c54b88f37e7441a56dfb5ddae170bcf
bulutcan99 <gocerbulutcan@gmail.com>
2024-06-03 17:53:38 +0300

    refactor: some polish

--- internal/adapter/transport/http/router.go
+++ internal/adapter/transport/http/router.go
@@ -14,17 +14,17 @@ func (s *server) authRouter() {
 }
 
 func (s *server) hrManagementRouter() {
-	route := s.app.Group("/hr-management", s.GetUserDetail)
-	route.Post("/create-user", s.CreateUser, s.RateLimiter(5, time.Minute), s.CheckPermission("HR", "add"))
-	route.Post("/create-department", s.CreateDepartment, s.RateLimiter(5, time.Minute), s.CheckPermission("HR", "add"))
+	route := s.app.Group("/hr-management", s.GetToken, s.GetUserDetail, s.RateLimiter(5, time.Minute))
+	route.Post("/create-user", s.CreateUser, s.CheckPermission("HR", "add"))
+	route.Post("/create-department", s.CreateDepartment, s.CheckPermission("HR", "add"))
 }
 
 func (s *server) carManagementRouter() {
-	route := s.app.Group("/car-management", s.GetUserDetail)
-	route.Post("/create-car", s.CreateCar, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
-	route.Post("/create-brand", s.CreateBrand, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
-	route.Post("/create-model", s.CreateModel, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
-	route.Post("/create-color", s.CreateColor, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
-	route.Post("/create-fuel", s.CreateFuel, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
-	route.Post("/create-transmission", s.CreateTransmission, s.RateLimiter(5, time.Minute), s.CheckPermission("Car", "add"))
+	route := s.app.Group("/car-management", s.GetToken, s.GetUserDetail, s.RateLimiter(5, time.Minute))
+	route.Post("/create-car", s.CreateCar, s.CheckPermission("Car", "add"))
+	route.Post("/create-brand", s.CreateBrand, s.CheckPermission("Car", "add"))
+	route.Post("/create-model", s.CreateModel, s.CheckPermission("Car", "add"))
+	route.Post("/create-color", s.CreateColor, s.CheckPermission("Car", "add"))
+	route.Post("/create-fuel", s.CreateFuel, s.CheckPermission("Car", "add"))
+	route.Post("/create-transmission", s.CreateTransmission, s.CheckPermission("Car", "add"))
 }
